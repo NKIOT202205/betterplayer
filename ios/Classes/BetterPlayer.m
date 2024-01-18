d2ef3fc0946bda274dcc776931327ea3b6f464ba
shiyu.pang <shiyu.pang@pactera.com>
2024-01-18 10:43:01 +0800

    FIX:ios重试时间6s -> 10s

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -285,7 +285,7 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
         [self play];
     } else {
         _stalledCount++;
-        if (_stalledCount > 6){
+        if (_stalledCount > 10){
             if (_eventSink != nil) {
                 _eventSink([FlutterError
                         errorWithCode:@"VideoError"
